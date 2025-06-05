1d8f8c735570aea5db5c679db7321fe8b49fbe51
AmirHossein <amir.h.ahmadnejad@gmail.com>
2025-06-05 15:27:16 -0400

    ignore update

--- build/lib/user_behavior_simulator/__init__.py
+++
@@ -1,7 +0,0 @@
-__version__ = "1.0.0"
-__author__ = "AmirHossein Ahmadnejad Roudsari"
-__email__ = "amirhahm@yorku.ca"
-
-from .simulator import UserBehaviorSimulator
-
-__all__ = ["UserBehaviorSimulator"]
